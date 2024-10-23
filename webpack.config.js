8c4bba767431abd0923e7e0bd29894b5169b201b
milotobias <tobiastap@gmail.com>
2024-10-23 10:13:30 +0200

    half finish

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,12 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'development',
   plugins: [new MiniCssExtractPlugin()],
   module: {
     rules: [
